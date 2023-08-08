1b77dbc7fe8a52eb1e629038ff8b4aeeb89389aa
fernandalisan <fernanda_lsantos@hotmail.com>
2023-08-08 20:00:11 -0300

    Arquivo app.py criado e inicializado

---
+++ app.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+def load_data():
+    return pd.read_csv('data/processed/bike_complete.csv')
+def main():
+    df =  load_data()
+    st.dataframe(df)
+if__name__== '__main__':
+    main()
